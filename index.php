10d33c9eb67bbef927be763e706b64afd47a8b3b
cerino68 <eric.grillon84@hotmail.fr>
2020-11-26 09:25:14 +0100

    initialisation squelette index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+ echo "<h1>BONJOUR</h1>" ;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Veille</h1>
+    <p>Savoir quoi faire quand on a pas tout compris....</p>
+    <ul>
+        <li><a href="article1.html">Sujet1</a></li>
+
+        <li><a href="article2.html">Sujet2</a></li>
+
+        <li><a href="article3.html">Sujet3</a></li>
+        
+    </ul>
+    
+</body>
+</html>
